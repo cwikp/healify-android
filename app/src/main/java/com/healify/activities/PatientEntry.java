74bfeef71fe32c5219c384caea60cb29679e3874
Kacper <kacperdyba@interia.pl>
2016-06-11 20:50:42 +0200

    Patient Entry Activity.

---
+++ app/src/main/java/com/healify/activities/PatientEntry.java
@@ -0,0 +1,15 @@
+package com.healify.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.healify.R;
+
+public class PatientEntry extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_patient_browse);
+    }
+}
