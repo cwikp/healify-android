1e926bcc01231e302da8e05e1a0f1c0ec78272a3
Piotr Ociepka <pkociepka@gmail.com>
2016-06-12 03:58:40 +0200

    Bind patient buttons with activities.

--- app/src/main/java/com/healify/activities/SinglePatient.java
+++ app/src/main/java/com/healify/activities/SinglePatient.java
@@ -7,6 +7,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.healify.R;
@@ -33,6 +34,32 @@ public class SinglePatient extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        Button updateButton = (Button) findViewById(R.id.buttonUpdate);
+        updateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(SinglePatient.this, PatientEntry.class));
+            }
+        });
+
+        Button healthstateButton = (Button) findViewById(R.id.buttonHealthState);
+        healthstateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(SinglePatient.this, PatientBrowse.class));
+            }
+        });
+
+//        Button checkoutButton = (Button) findViewById(R.id.buttonCheckOut);
+//        checkoutButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                startActivity(new Intent(SinglePatient.this, CheckOutPopup.class));
+//            }
+//        });
+
+
 
         Intent intent = getIntent();
         patientDTO = (PatientDTO) intent.getSerializableExtra("patient");
--- app/src/main/java/com/healify/activities/patientBrowse.java
+++ app/src/main/java/com/healify/activities/patientBrowse.java
@@ -3,6 +3,7 @@ package com.healify.activities;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Button;
 
@@ -30,6 +31,15 @@ public class PatientBrowse extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(PatientBrowse.this, DrugsListingPopup.class));
+            }
+        });
+
+        // checkups popup
+        Button checkupsButton = (Button) findViewById(R.id.check_button);
+        checkupsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(PatientBrowse.this, CheckupListingPopup.class));
             }
         });
     }
